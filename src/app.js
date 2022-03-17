ae3c3c1e934ccb5519397fe41e66742ec4d9d27b
PAHEERATHAN KEERAN <keeran.paheerathan@inrae.fr>
2022-03-17 16:14:50 +0100

    Fixed interface pointer offset

--- src/app.js
+++ src/app.js
@@ -2,15 +2,13 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui'
 
-let camera, scene, renderer, canvas, points;
+let camera, scene, renderer, canvas, points, raycaster, mouse;
 
 export const init = () => {
-    const mouse = {
+    mouse = {
         x: undefined,
         y: undefined
     }
-
-
 
     canvas = document.querySelector("#canvas")
     renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
@@ -28,17 +26,20 @@ export const init = () => {
     controls.maxDistance = 10;
     controls.listenToKeyEvents(window)
 
-    // const gui = dat.GUI()
+    const gui = new dat.GUI()
 
-    // const world = {
-    //     brush: {
-    //         size: 100
-    //     }
-    // }
+    const world = {
+        brush: {
+            size: 0.05
+        }
+    }
 
-    // // gui.add(world.brush, 'size', 1, 500)
+    gui.add(world.brush, 'size', 0.005, 0.5).onChange(() => {
+        raycaster.params.Points.threshold = world.brush.size
+    })
 
-    const raycaster = new THREE.Raycaster()
+    raycaster = new THREE.Raycaster()
+    raycaster.params.Points.threshold = 0.05
 
     const animate = () => {
         requestAnimationFrame(animate)
@@ -49,72 +50,67 @@ export const init = () => {
             camera.aspect = canvas.clientWidth / canvas.clientHeight;
             camera.updateProjectionMatrix();
         }
-
-
-
-        animate()
-
-        window.addEventListener('keypress', keyboard);
-        addEventListener('mousemove', (event) => {
-            mouse.x = (event.clientX / window.innerWidth) * 2 - 1
-            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
-        })
     }
 
-    export const displayPoints = (_points) => {
-        points = _points
-        points.geometry.center();
-        points.geometry.rotateX(Math.PI);
-        points.material.color.set(0xffffff)
-        points.material.vertexColors = true
+    animate()
 
-        const colors = []
-
-        for (let i = 0; i < points.geometry.attributes.position.count; i++) {
-            colors.push(0, 0, 0)
-        }
-
-        points.geometry.setAttribute("color", new THREE.BufferAttribute(new Float32Array(colors), 3))
-        console.log(points.geometry)
-        scene.add(points);
-    }
-
-    function keyboard(ev) {
-        switch (ev.key || String.fromCharCode(ev.keyCode || ev.charCode)) {
-            case '+':
-                points.material.size *= 1.2;
-                break;
-
-            case '-':
-                points.material.size /= 1.2;
-                break;
-
-            case ' ':
-                raycaster.setFromCamera(mouse, camera)
-                raycaster.params.Points.threshold = 0.05;
-                if (points) {
-                    const intersects = raycaster.intersectObject(points, false)
-
-                    if (intersects.length > 0) {
-                        console.log(intersects.length)
-                        for (let i = 0; i < intersects.length; i++) {
-                            const { color } = intersects[i].object.geometry.attributes
-
-                            color.setX(intersects[i].index, 0)
-                            color.setY(intersects[i].index, 0.56)
-                            color.setZ(intersects[i].index, 0.56)
-                            color.needsUpdate = true
-                        }
-                    }
-                }
-        }
-    }
+    window.addEventListener('keypress', keyboard);
+    addEventListener('mousemove', (event) => {
+        mouse.x = ((event.clientX - document.querySelector("#nav").clientWidth) / canvas.clientWidth) * 2 - 1
+        mouse.y = -(event.clientY / canvas.clientHeight) * 2 + 1
+    })
 }
 
-// export function render() {
+export const displayPoints = (_points) => {
+    points = _points
+    points.geometry.center()
+    points.geometry.rotateX(Math.PI)
+    points.material.color.set(0xffffff)
+    points.material.vertexColors = true
 
-//     renderer.render(scene, camera);
-// }
+    const colors = []
+
+    for (let i = 0; i < points.geometry.attributes.position.count; i++) {
+        colors.push(0, 0, 0)
+    }
+
+    points.geometry.setAttribute("color", new THREE.BufferAttribute(new Float32Array(colors), 3))
+    console.log(points.geometry)
+    scene.add(points);
+}
+
+function keyboard(ev) {
+    switch (ev.key || String.fromCharCode(ev.keyCode || ev.charCode)) {
+        case '+':
+            points.material.size *= 1.2
+            break
+
+        case '-':
+            points.material.size /= 1.2
+            break
+
+        case ' ':
+            raycaster.setFromCamera(mouse, camera)
+            if (points) {
+                const intersects = raycaster.intersectObject(points, false)
+
+                if (intersects.length > 0) {
+                    for (let i = 0; i < intersects.length; i++) {
+                        const { color } = intersects[i].object.geometry.attributes
+                        color.setX(intersects[i].index, 0)
+                        color.setY(intersects[i].index, 0.56)
+                        color.setZ(intersects[i].index, 0.56)
+                        color.needsUpdate = true
+
+                    }
+                }
+            }
+
+            break
+    }
+
+
+}
 
 function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import { PCDLoader } from 'three/examples/jsm/loaders/PCDLoader.js'
 import { displayPoints, init } from './app';
 
-
 init()
 
-// document.getElementById("selectFile").oninput = (e) => {
-//     const file = e.target.files[0]
-//     const fileExtenson = file.name.split('.')[1]
-// if (fileExtenson == "pcd") {
-//         const fileURL = URL.createObjectURL(file)
-const loader = new PCDLoader();
+document.getElementById("selectFile").oninput = (e) => {
+    const file = e.target.files[0]
+    const fileExtenson = file.name.split('.')[1]
+    if (fileExtenson == "pcd") {
+        const fileURL = URL.createObjectURL(file)
+        const loader = new PCDLoader();
 
-loader.load('../data/test.pcd', points => {
-        displayPoints(points)
-    })
-    //     } else {
-    //         alert("Please select a .pcd file")
-    //     }
-    // }
+        loader.load(fileURL, points => {
+            displayPoints(points)
+        })
+    } else {
+        alert("Please select a .pcd file")
+    }
+}
